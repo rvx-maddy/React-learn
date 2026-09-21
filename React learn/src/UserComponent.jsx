af878260cb205b7c4c3f778026f873070716b283
rvx-maddy <kunwarbibek6661@gmail.com>
2026-09-21 16:17:18 +0545

    Importing and Exporting Components

---
+++ React learn/src/UserComponent.jsx
@@ -0,0 +1,25 @@
+function Login() {
+    return (
+        <>
+            <h1>Login User </h1>
+        </>
+    )
+}
+
+
+export function Profile() {
+    return (
+        <>
+            <h1>Profile User </h1>
+        </>
+    )
+}
+
+export function Setting() {
+    return (
+        <>
+            <h1>Profile Setting </h1>
+        </>
+    )
+}
+export default Login
